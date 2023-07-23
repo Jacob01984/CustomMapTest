d3d48d5eee6f0abd58c4063476cbb985d7f4daf1
Jacob01984 <Jacoblavenant@yahoo.com>
2023-07-23 14:26:50 -0500

    Initial Commit

---
+++ CustomMapTest/CustomMapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMapTestApp.swift
+//  CustomMapTest
+//
+//  Created by Jacob Lavenant on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomMapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
